3413d33b57eb01d79c1db6cccb9fe03e443971b5
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-06-07 00:12:20 -0300

    Expansion

--- appendix/funcoes.sh
+++ appendix/funcoes.sh
@@ -9,3 +9,15 @@ funcao_com_argumentos() {
 funcao_sem_argumentos
 
 funcao_com_argumentos um dois tres
+
+o=+ a=1 z=${1:-1}
+[ "$2" ] && {
+    a=$1
+    z=$2
+}
+[ $a -gt $z ] && o=-
+while [ $a -ne $z ]; do
+    echo $a
+    eval "a=\$((a $o 1))"
+done
+echo $a
