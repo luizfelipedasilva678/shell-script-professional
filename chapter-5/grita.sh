a6eaf13ed063411a2712de48a5fdefc7079e2eff
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-06-21 22:34:10 -0300

    Debug

--- chapter-5/grita.sh
+++ chapter-5/grita.sh
@@ -1,10 +1,18 @@
 TXT="gritaria"
 TXT="    $TXT     "
+DEBUG=1
+
+Debug() {
+    [ "$DEBUG" = 1 ] && echo "$*"
+}
+
+Debug "TXT com espaços:  [$TXT]"
 
 set -x
+trap read DEBUG
 echo "TXT com espaços    : [$TXT]"
-set +x
 TXT=$(echo "$TXT" | tr ' ' '!')
+trap "" DEBUG
 echo "TXT com exclamações: [$TXT]"
 TXT=$(echo "$TXT" | tr a-z A-Z)
 
