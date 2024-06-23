c5a0142d2a8462e6c716bb16d6815511fee4a3a6
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-06-22 21:16:56 -0300

    Control characters

---
+++ chapter-6/control-characters-2.sh
@@ -0,0 +1,2 @@
+echo -e '\033[31m Mensagem Importante!! \033[m'
+echo -e '\033[44;31;1;5m Mensagem Importante!! \033[m'
---
+++ chapter-6/cores.sh
@@ -0,0 +1,11 @@
+for letra in 0 1 2 3 4 5 6 7 8 9; do
+    for brilho in '' ';1'; do
+        for fundo in 0 1 2 3 4 5 6 7 8 9; do
+            seq="4$fundo;3$letra"
+            echo -e "\033[$seq${brilho}m\c"
+            echo -e " $seq${brilho:- } \c"
+            echo -e "\033[m\c"
+        done
+        echo
+    done
+done
