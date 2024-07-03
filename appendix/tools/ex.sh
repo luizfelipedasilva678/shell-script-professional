c4e2a7c7f6bbad3b4541699ca3b405d14e6c84d9
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-07-02 23:12:34 -0300

    Tools

---
+++ appendix/tools/ex.sh
@@ -0,0 +1,8 @@
+# echo -e "um\ndois\n\n\n\ntrês"
+# echo -e "um\ndois\n\n\n\ntrês" | cat -n -
+# echo -e "um\ndois\n\n\n\ntrês" | cat -n -s -
+# echo "um:dois:três:quatro:cinco" | cut -d : -f 2,3,4
+# date
+# date -d yesterday
+# echo -e "\a"
+find /etc -iname 'x*'
